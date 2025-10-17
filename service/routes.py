86593a8ac2f34e51b90c1e91cf0483756829115b
Nico Medina <nico@nlmedina.com>
2025-10-17 09:44:43 -0700

    add available query support to list products endpoint

--- service/routes.py
+++ service/routes.py
@@ -112,6 +112,7 @@ def list_products():
     products = []
     name = request.args.get("name")
     category = request.args.get("category")
+    available = request.args.get("available")
 
     if name:
         app.logger.info("Find by name: %s", name)
@@ -121,6 +122,11 @@ def list_products():
         # create enum from string
         category_value = getattr(Category, category.upper())
         products = Product.find_by_category(category_value)
+    elif available:
+        app.logger.info("Find by available: %s", available)
+        # create bool from string
+        available_value = available.lower() in ["true", "yes", "1"]
+        products = Product.find_by_availability(available_value)
     else:
         app.logger.info("Find all")
         products = Product.all()
