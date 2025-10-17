513d07b3cda3b820cf85a81ea71d0d6b5912c999
Nico Medina <nico@nlmedina.com>
2025-10-17 09:35:24 -0700

    add get product list unit test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -205,6 +205,14 @@ class TestProductRoutes(TestCase):
         data = response.get_json()
         self.assertIn("was not found", data["message"])
 
+    def test_get_product_list(self):
+        """It should Get a list of Products"""
+        self._create_products(5)
+        response = self.client.get(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), 5)
+
     ######################################################################
     # Utility functions
     ######################################################################
