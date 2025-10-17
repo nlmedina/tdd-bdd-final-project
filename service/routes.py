4cfdd018534cc5f926cc88a0a5c134610e3343bd
Nico Medina <nico@nlmedina.com>
2025-10-17 09:32:53 -0700

    add delete product route handler

--- service/routes.py
+++ service/routes.py
@@ -158,10 +158,16 @@ def update_products(product_id):
 
 
 ######################################################################
-# D E L E T E   A   P R O D U C T
+# DELETE A PRODUCT
 ######################################################################
-
-
-#
-# PLACE YOUR CODE TO DELETE A PRODUCT HERE
-#
+@app.route("/products/<int:product_id>", methods=["DELETE"])
+def delete_products(product_id):
+    """
+    Delete a Product
+    This endpoint will delete a Product based the id specified in the path
+    """
+    app.logger.info("Request to Delete a product with id [%s]", product_id)
+    product = Product.find(product_id)
+    if product:
+        product.delete()
+    return "", status.HTTP_204_NO_CONTENT
